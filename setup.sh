78722b1350e187cc8c225da5f1f9c3d336eccb01
Christian Holzberger <ch@mosaik-software.de>
2015-12-02 00:04:52 +0100

    Default SOGO Config and ENV to Config mappings

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 sogo_dbhost=${$SOGO_DB_HOST:-mysql\://sogo\:sogo@sogo-mysql:3306}
 sogo_timezone=$($SOGO_TIMEZONE:-Europe/Amsterdam)
 
-sed -e "s/\[\[SOGO_DB_HOST\]\]/$sogo_dbhost/g" /etc/sogo/sogo.conf.template \
- -e "s/\[\[SOGO_TIMEZONE\]\]/$sogo_timezone/g" /etc/sogo/sogo.conf.template > /etc/sogo/sogo.conf
+sed -e "s/\[\[SOGO_DB_HOST\]\]/$sogo_dbhost/g" \
+ -e "s/\[\[SOGO_TIMEZONE\]\]/$sogo_timezone/g" \
+ /etc/sogo/sogo.conf.template > /etc/sogo/sogo.conf
 
