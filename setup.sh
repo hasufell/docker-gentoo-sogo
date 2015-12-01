98d9b19e04521f5bc8426ef00429e033c5955b37
Christian Holzberger <ch@mosaik-software.de>
2015-12-02 00:44:09 +0100

    Add imap authentication and configuration

--- setup.sh
+++ setup.sh
@@ -5,7 +5,13 @@
 sogo_dbhost=${SOGO_DB_HOST:-mysql\://sogo\:sogo@sogo-mysql\:3306}
 sogo_timezone=${SOGO_TIMEZONE:-Europe/Amsterdam}
 
+# IMAP SERVER SETUP - WILL AUTHENTICATE AGAINS IT
+sogo_imaphost=${SOGO_IMAPHOST:-imap://localhost}
+
+
+
 sed -e "s;\[\[SOGO_DB_HOST\]\];$sogo_dbhost;g" \
  -e "s;\[\[SOGO_TIMEZONE\]\];$sogo_timezone;g" \
+ -e "s;\[\[SOGO_IMAP_HOST\]\];$sogo_imaphost;g" \
  /etc/sogo/sogo.conf.template > /etc/sogo/sogo.conf
 
